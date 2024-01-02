02ccbb1f3f8a71fd2ee87b1a0a8b3db6e1dc65cd
its0xRay <99510382+its0xRay@users.noreply.github.com>
2024-01-02 23:45:19 +0530

    v0.11

--- src/cli.rs
+++ src/cli.rs
@@ -15,7 +15,16 @@ pub enum Action {
     List,
 }
 
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "Journal",
+    about = "A command line to-do app written in Rust"
+)]
+
 pub struct CommandLineArgs {
+    #[structopt(subcommand)]
     pub action: Action,
+
+    #[structopt(parse(from_os_str), short, long)]
     pub journal_file: Option<PathBuf>,
 }
